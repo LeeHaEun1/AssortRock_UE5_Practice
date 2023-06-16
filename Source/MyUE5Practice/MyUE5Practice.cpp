9ad1566347f3b33a4b61086ca35ae42897b34c8c
Woo <tnwjddl9606@gmail.com>
2023-06-16 11:17:11 +0900

    HW_0616 완료

---
+++ Source/MyUE5Practice/MyUE5Practice.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyUE5Practice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyUE5Practice, "MyUE5Practice" );
